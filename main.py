fe9f529d1b59c61690ab3da3db20626da803ca86
ngocminhta <86149296+ngocminhta@users.noreply.github.com>
2023-03-23 16:31:03 +0700

    second commit

--- main.py
+++ main.py
@@ -1,50 +1,82 @@
-# This is a sample Python script.
+import json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from urllib.request import urlopen, urlretrieve
-import cgi
 import requests
 import os
+import logging
+
+global date
+def readDateHistory():
+    global date
+    with open('dateHistory.json', 'r') as f:
+        date = json.load(f)
+
+def downloadNewDate(lastDate, cur):
+    for i in range(date[lastDate]+1,5383):
+        try:
+            cvDate = str(i)
+            print(cvDate)
+            URL = "https://links.sgx.com/1.0.0/derivatives-historical/" + cvDate + "/WEBPXTICK_DT.zip"
+            response = requests.get(URL)
+            header = response.headers.get('Content-Disposition')
+            print(header[-4:])
+            if header[-4:] == '.zip':
+                name = header[-12:-8] + '-' + header[-8:-6] + '-' + header[-6:-4]
+                print(name)
+                date[name] = i
+            elif header[-5:-1] == '.tic':
+                date[header[-17:-13] + '-' + header[-13:-11] + '-' + header[-11:-9]] = i
+            elif header[-4:-1] == '.gz':
+                date[header[-12:-8] + '-' + header[-8:-6] + '-' + header[-6:-4]] = i
+            elif header[-5:-1] == '.txt':
+                date[header[-17:-13] + '-' + header[-13:-11] + '-' + header[-11:-9]] = i
+        except:
+            continue
+    print(date)
 
 def downloadFile():
     # Create folder
     directory = getDate()
-    #os.mkdir(directory)
-    # cvDate = str(5380)
-    # files = ["/WEBPXTICK_DT.zip", "/TickData_structure.dat", "/TC.txt", "/TC_structure.dat"]
-    # URLpart = "https://links.sgx.com/1.0.0/derivatives-historical/"
-    #
-    # for file in files:
-    #     response = requests.get(URLpart + cvDate + file)
-    #     header = response.headers.get('Content-Disposition')
-    #     open(directory + file, "wb").write(response.content)
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+    cvDate = str(date[directory])
+    print(cvDate)
+    files = ["/WEBPXTICK_DT.zip", "/TickData_structure.dat", "/TC.txt", "/TC_structure.dat"]
+    URLpart = "https://links.sgx.com/1.0.0/derivatives-historical/"
 
-    for i in range(2709,5380):
-        try:
-            cvDate = str(i)
-            URL = "https://links.sgx.com/1.0.0/derivatives-historical/" + cvDate + "/WEBPXTICK_DT.zip"
-            response = requests.get(URL)
-            header = response.headers.get('Content-Disposition')
-            open(directory + "/filename", "a").write(header + "\n")
-        except:
-            open(directory + "/filename", "a").write("No data found!" + "\n")
+    for file in files:
+        response = requests.get(URLpart + cvDate + file)
+        header = response.headers.get('Content-Disposition')
+        open(directory + file, "wb").write(response.content)
+
+    # get the date assigned to the file
+    # for i in range(2709,5380):
+    #     try:
+    #         cvDate = str(i)
+    #         URL = "https://links.sgx.com/1.0.0/derivatives-historical/" + cvDate + "/WEBPXTICK_DT.zip"
+    #         response = requests.get(URL)
+    #         header = response.headers.get('Content-Disposition')
+    #         open(directory + "/filename", "a").write(header + "\n")
+    #     except:
+    #         open(directory + "/filename", "a").write("No data found!" + "\n")
+
 
 def getDate():
-    print('Type the day you want to download in format yyyy-mm-dd:')
-    date = input()
-    year = int(date[:4])
-    month = int(date[5:7])
-    day = int(date[8:])
-    return date
+    global date
+    cur = input('Type the day you want to download in format yyyy-mm-dd:\n')
+    if cur > list(date)[-1]:
+        downloadNewDate(list(date)[-1],cur)
+    return cur
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    downloadFile()
-    # try:
-    #     getDate()
-    # except:
-    #     print('Your date you typed is not valid. Please try again.')
-    #     getDate()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    readDateHistory()
+    while True:
+        downloadFile()
+        # try:
+        #     downloadFile()
+        # except:
+        #     print('Your date you typed is not valid. Please try again.')
+        #     downloadFile()
+        checkEnd = input("Do you want to download another day? (Y/N)\n")
+        if checkEnd == "N":
+            break
